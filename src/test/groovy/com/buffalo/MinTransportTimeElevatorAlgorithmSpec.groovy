1c1832d7ba453cb10e3c5f3411c6bf7ea6410d9d
vladimir.selivanov <vladimir.selivanov@gmail.com>
2018-10-30 07:37:59 +0700

    Расширен набор тестов

--- src/test/groovy/com/buffalo/MinTransportTimeElevatorAlgorithmSpec.groovy
+++ src/test/groovy/com/buffalo/MinTransportTimeElevatorAlgorithmSpec.groovy
@@ -2,6 +2,8 @@ package com.buffalo
 
 import com.buffalo.model.State
 import com.buffalo.transport.Command
+import com.buffalo.transport.Direction
+import org.apache.commons.collections4.CollectionUtils
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -21,7 +23,6 @@ class MinTransportTimeElevatorAlgorithmSpec extends Specification {
 
         when:
         int actualCost = elevatorAlgorithm.getCost(states, command)
-        System.out.println("cost 1, 1 -> 9 = " + cost)
 
         then:
         actualCost == cost
@@ -30,5 +31,51 @@ class MinTransportTimeElevatorAlgorithmSpec extends Specification {
         from | to || cost
         1    | 9  || 40
         9    | 1  || 40
+    }
+
+    def "Прямой маршрут с остановкой #from -> #stop -> #to"() {
+        given:
+        def states = new ArrayList<>()
+        states.add(new State(from, from, LocalDateTime.now()))
+        addRoute(states, from, stop)
+        addRoute(states, stop, to)
+        Command command = new Command(from, to)
+
+        when:
+        int actualCost = elevatorAlgorithm.getCost(states, command)
+
+        then:
+        actualCost == cost
+
+        where:
+        from | stop | to || cost
+        1    | 4    | 5  || 40
+        5    | 4    | 1  || 40
+    }
+
+    void addState(List<State> states, int from, int to) {
+        LocalDateTime now = LocalDateTime.now()
+        LocalDateTime start = now
+        if (CollectionUtils.isNotEmpty(states)) {
+            State lastState = states.get(states.size() - 1)
+            // todo selivanov: тут может быть ошибка при построении виртуального графика
+            if (lastState.getDirection() != Direction.STOP || now.isBefore(lastState.getEnd())) {
+                start = lastState.getEnd()
+            }
+        }
+        states.add(new State(from, to, start))
+    }
+
+    void addRoute(List<State> states, int from, int to) {
+        if (from < to) {
+            for (int i = from; i < to; i++) {
+                addState(states, i, i + 1)
+            }
+        } else if (from > to) {
+            for (int i = from; i > to; i--) {
+                addState(states, i, i - 1)
+            }
+        }
+        addState(states, to, to)
     }
 }
